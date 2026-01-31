5efd8cc8aa5f5b374f6871955792fbf0ff0c1c1b
fardin0045 <fardinonik40@gmail.com>
2026-01-31 19:50:40 +0600

    Initial Database setup done

---
+++ StudentPortal/Data/ApplicationsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortal.Models.Entites;
+
+namespace StudentPortal.Data
+{
+    public class ApplicationsDbContext :DbContext
+    {
+        public ApplicationsDbContext(DbContextOptions<ApplicationsDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Student> Students{ get; set; }
+    }
+}
