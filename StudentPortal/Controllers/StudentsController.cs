70761d079040708d4ec7afe630145deba7a526b3
fardin0045 <fardinonik40@gmail.com>
2026-01-31 20:43:56 +0600

    Data Save to the database

--- StudentPortal/Controllers/StudentsController.cs
+++ StudentPortal/Controllers/StudentsController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Data;
+using StudentPortal.Models;
+using StudentPortal.Models.Entites;
 
 namespace StudentPortal.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly ApplicationsDbContext dbContext;
+        public StudentsController(ApplicationsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         [HttpGet]
         public IActionResult Add()
         {
+            return View();
+        }
+        [HttpPost]
+        public async  Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribe = viewModel.Subscribe,
+            };
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
             return View();
         }
     }
