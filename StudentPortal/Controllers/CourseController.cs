b5bc5d1d57ee3ded169ca1d965f496f5b8a9c927
fardin0045 <fardinonik40@gmail.com>
2026-02-08 22:19:39 +0600

    Course Add fcuntion is done

---
+++ StudentPortal/Controllers/CourseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Data;
+using StudentPortal.Models;
+using StudentPortal.Models.Entites;
+
+namespace StudentPortal.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ApplicationsDbContext dbContext;
+        public CourseController(ApplicationsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost ]
+        public async Task<IActionResult> Add(AddCourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid) { 
+                return View(viewModel);
+            }
+            var course = new Courses
+            {
+                CourseName = viewModel.CourseName,
+                CourseCode = viewModel.CourseCode,
+                Credit = viewModel.Credit
+            };
+
+            await dbContext.Courses.AddAsync(course);
+            await dbContext.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = "Course Added Successfully";
+            return RedirectToAction("Add");
+        }
+
+    }
+}
---
+++ StudentPortal/Models/AddCourseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentPortal.Models
+{
+    public class AddCourseViewModel
+    {
+        public string CourseName { get; set; }
+        public string CourseCode {  get; set; }
+        public int Credit {  get; set; }
+    }
+}
