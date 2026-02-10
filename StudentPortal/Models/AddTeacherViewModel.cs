182512cda2581da703d968c016b556b211836ec7
fardin0045 <fardinonik40@gmail.com>
2026-02-10 14:29:00 +0600

    Started the CRUD for teacher

---
+++ StudentPortal/Models/AddTeacherViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentPortal.Models
+{
+    public class AddTeacherViewModel
+    {
+        public string TeacherName { get; set; }
+        public string TeacherEmail { get; set; }
+        public string TeacherPhone { get; set; }
+    }
+}
---
+++ StudentPortal/Models/Entites/Teachers.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentPortal.Models.Entites
+{
+    public class Teachers
+    {
+    
+        public Guid TeacherId { get;set; }  
+        public string TeacherName { get;set; }
+        public string TeacherEmail { get;set; }
+        public string TeacherPhone { get;set; }
+
+
+    }
+}
